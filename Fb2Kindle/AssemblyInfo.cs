fb335c98cbc4dcee1b10503fb54628a8fc1a4c48
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2025-06-18 21:25:28 +0300

    cleanup

--- Fb2Kindle/AssemblyInfo.cs
+++ Fb2Kindle/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Fb2Kindle book converter")]
@@ -7,13 +6,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("fb2kindle")]
 [assembly: AssemblyProduct("fb2kindle")]
-[assembly: AssemblyCopyright("Copyright © Sergiy Yegoshyn 2021")]
+[assembly: AssemblyCopyright("Copyright © Sergiy Yegoshyn 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("75655dff-13a8-4f14-b340-82d93a46d4cb")]
 
-[assembly: InternalsVisibleTo("jail")]
+//[assembly: InternalsVisibleTo("jail")]
 
 [assembly: AssemblyVersion("2.1.*")]
