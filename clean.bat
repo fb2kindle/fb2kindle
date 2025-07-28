170dc0665abb757f974801a77da59be5c6c51033
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2025-07-28 23:06:52 +0300

    cleanup

--- clean.bat
+++ clean.bat
@@ -9,14 +9,4 @@ rmdir /s /q .\Fb2Kindle\obj
 rmdir /s /q .\Fb2Thumbnails\bin
 rmdir /s /q .\Fb2Thumbnails\obj
 
-rmdir /s /q .\LibraryCleaner\bin
-rmdir /s /q .\LibraryCleaner\obj
-
-rmdir /s /q .\other\PrettyXml\bin
-rmdir /s /q .\other\PrettyXml\obj
-
-rmdir /s /q .\jail\obj
-del /S ".\jail\bin\*.pdb"
-del /S ".\jail\bin\*.xml"
 del /S ".\Fb2Kindle\FodyWeavers.xsd"
-del /S ".\LibraryCleaner\FodyWeavers.xsd"
